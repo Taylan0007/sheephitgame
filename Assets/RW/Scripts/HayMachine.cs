8f1aed7c7afa1dacaa59c371f03cda07263ee5be
Taylan <armutsurat123@gmail.com>
2022-03-29 11:27:00 +0300

    first commit

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+
+    public GameObject hayBalePrefab;
+    public Transform haySpawnpoint;
+    public float ShootInterval;
+    public float ShootTimer;
+
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput < 0 && transform.position.x >-horizontalBoundary)
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if(horizontalInput > 0 && transform.position.x < horizontalBoundary)
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+    private void UpdateShooting()
+    {
+        ShootTimer -= Time.deltaTime;
+        if (ShootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            ShootTimer = ShootInterval;
+            ShootHay();
+        }
+    }
+}
